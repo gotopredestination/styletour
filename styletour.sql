23c6aab28a86d24372386e20194ac6db8cac8e46
gotopredestination <shp.vova@mail.ru>
2017-06-09 17:49:04 +0300

    обновлена база данных

--- styletour.sql
+++ styletour.sql
@@ -1,11 +1,11 @@
 -- phpMyAdmin SQL Dump
--- version 4.0.10.6
--- http://www.phpmyadmin.net
+-- version 4.6.5.2
+-- https://www.phpmyadmin.net/
 --
--- Host: 127.0.0.1:3307
--- Generation Time: Jun 02, 2017 at 09:46 PM
--- Server version: 5.5.41-log
--- PHP Version: 5.6.3
+-- Хост: 127.0.0.1:3306
+-- Время создания: Июн 09 2017 г., 17:43
+-- Версия сервера: 5.7.16
+-- Версия PHP: 5.6.29
 
 SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
 SET time_zone = "+00:00";
@@ -14,31 +14,42 @@ SET time_zone = "+00:00";
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
 /*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8 */;
+/*!40101 SET NAMES utf8mb4 */;
 
 --
--- Database: `styletour`
+-- База данных: `styletour`
 --
 
 -- --------------------------------------------------------
 
 --
--- Table structure for table `maintexts`
+-- Структура таблицы `articles`
 --
 
-CREATE TABLE IF NOT EXISTS `maintexts` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
+CREATE TABLE `articles` (
+  `id` int(11) NOT NULL,
+  `nameArticle` varchar(255) NOT NULL,
+  `descriptionArticle` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `maintexts`
+--
+
+CREATE TABLE `maintexts` (
+  `id` int(11) NOT NULL,
   `name` tinytext NOT NULL,
   `body` text NOT NULL,
   `url` tinytext NOT NULL,
   `showhide` enum('show','hide') NOT NULL,
   `lang` enum('ru','en') NOT NULL,
-  `putdate` date DEFAULT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=4 ;
+  `putdate` date DEFAULT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `maintexts`
+-- Дамп данных таблицы `maintexts`
 --
 
 INSERT INTO `maintexts` (`id`, `name`, `body`, `url`, `showhide`, `lang`, `putdate`) VALUES
@@ -49,22 +60,101 @@ INSERT INTO `maintexts` (`id`, `name`, `body`, `url`, `showhide`, `lang`, `putda
 -- --------------------------------------------------------
 
 --
--- Table structure for table `support`
+-- Структура таблицы `support`
 --
 
-CREATE TABLE IF NOT EXISTS `support` (
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `name` text NOT NULL,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB  DEFAULT CHARSET=utf8 AUTO_INCREMENT=2 ;
+CREATE TABLE `support` (
+  `id` int(11) NOT NULL,
+  `name` text NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
 --
--- Dumping data for table `support`
+-- Дамп данных таблицы `support`
 --
 
 INSERT INTO `support` (`id`, `name`) VALUES
 (1, '<p>Priver</p>');
 
+-- --------------------------------------------------------
+
+--
+-- Структура таблицы `users`
+--
+
+CREATE TABLE `users` (
+  `id` int(11) NOT NULL,
+  `login` tinytext NOT NULL,
+  `password` int(11) NOT NULL,
+  `email` tinytext NOT NULL,
+  `datereg` date NOT NULL,
+  `lastvisit` datetime NOT NULL,
+  `blockunblock` enum('unblock','block') NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Дамп данных таблицы `users`
+--
+
+INSERT INTO `users` (`id`, `login`, `password`, `email`, `datereg`, `lastvisit`, `blockunblock`) VALUES
+(14, 'lalalalalalala', 777, 'mail@mail.ru', '2017-06-07', '2017-06-07 20:37:31', 'unblock'),
+(15, '', 0, '', '2017-06-07', '2017-06-07 20:38:33', 'unblock'),
+(16, 'lalalalalalala4', 123, 'mail@mail.ru', '2017-06-07', '2017-06-07 20:38:53', 'unblock'),
+(17, '', 0, '', '2017-06-07', '2017-06-07 20:40:15', 'unblock'),
+(18, '', 0, '', '2017-06-07', '2017-06-07 20:40:23', 'unblock'),
+(19, 'asdfation', 123, 'maail@mail.ruasdf', '2017-06-07', '2017-06-07 20:51:45', 'unblock');
+
+--
+-- Индексы сохранённых таблиц
+--
+
+--
+-- Индексы таблицы `articles`
+--
+ALTER TABLE `articles`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `maintexts`
+--
+ALTER TABLE `maintexts`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `support`
+--
+ALTER TABLE `support`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- Индексы таблицы `users`
+--
+ALTER TABLE `users`
+  ADD PRIMARY KEY (`id`);
+
+--
+-- AUTO_INCREMENT для сохранённых таблиц
+--
+
+--
+-- AUTO_INCREMENT для таблицы `articles`
+--
+ALTER TABLE `articles`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT для таблицы `maintexts`
+--
+ALTER TABLE `maintexts`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+--
+-- AUTO_INCREMENT для таблицы `support`
+--
+ALTER TABLE `support`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=2;
+--
+-- AUTO_INCREMENT для таблицы `users`
+--
+ALTER TABLE `users`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=20;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
